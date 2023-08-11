6f4dca257eb06244e859321a45c8fb0bedea2731
Bruno-Melo-Martins <133404191+Bruno-Melo-Martins@users.noreply.github.com>
2023-08-11 17:21:49 -0300

    Initial commit

---
+++ 11 de agosto/Atividade/JS/node.js
@@ -0,0 +1,35 @@
+/* 1º exercicio */
+document.write('1<br>')
+let a = 1, b = 2, c = 3
+document.write('Numeros: ' + a + " " + b + " " + c)
+
+
+/* 2º exercicio */
+document.write('<br>2<br>')
+let num1 = prompt('Digite o primeiro número:'),
+num2 = prompt('Digite o segundo número:'),
+num3 = prompt('Digite o terceiro número:')
+document.write('Primeiro número: ' + num1 + '<br>Segundo número: ' + num2 + '<br>Terceiro número: ' + num3)
+
+/* 3º exercicio */
+console.log('<br>3<br>')
+let nome = prompt('Digite seu nome:')
+let idade =  prompt('Digite sua idade:')
+console.log('Olá, meu nome é ' + nome + 'e eu tenho ' + idade + ' anos')
+
+/* 4º exercicio */
+document.write('<br>4<br>')
+let n1 = prompt('Digite o primeiro número:')
+let n2 = prompt('Digite o segundo número:')
+parseInt(n1)
+parseInt(n2)
+let soma = n1 + n2
+let subt = n1 - n2
+let mult = n1 * n2
+let divi = n1/n2
+document.write(
+    'Subtração: ' + subt +
+    '<br>Soma: ' + soma +
+    '<br>Multiplicação: ' + mult +
+    '<br>Divisão: ' + divi
+    )
---
+++ 11 de agosto/JS/node.js
@@ -0,0 +1,54 @@
+/*let re = prompt('Mãe tem café?')
+        if (re == "Tem não.")
+        {
+            alert('pois vo come aboba com leite');
+        } else{
+            alert('Mesmo assim vou comer aboba com leite');
+        }*/
+//Comentário de uma linha
+
+/*
+let carac = "aula de js" //declaração de caracter
+let vouf = true //declaração de booleanos
+let num = 13 //declaração do tipo numeros
+
+//a seguir mostro em console o valor das variaveis:
+
+console.log(carac)
+
+//para fazer aparecer uma variavel à um usuario:
+
+document.write(carac)
+
+//por meio de dialogos:
+
+//alert("Its been a long long time")
+*/
+
+/*
+//contatenação de variaveis
+let text = " tem "
+let nome = prompt('Digite seu nome:')
+let idade =  prompt('Digite seu nome:')
+document.write(nome + text + idade + ' anos')
+*/
+
+/*DESAFIO
+Elabore um programa que
+tenha 3 variaveis (a b c)
+a seguir imprima na tela pro 
+usuario o valor das variaveis
+depois seu programa deve utlizar
+uma lógica e inverter os valores de
+a e b*/
+
+let a = 19
+let b = 14
+let c = 34
+document.write('Os valores de a, b e c são, respectivamente: ' + a + ", " + b + " e " + c)
+c = a
+a = b
+b = c
+document.write("<br>Os valores de a, b agora são, respectivamente: " + a + " e " + b)
+
+let nome = voaida, numero = 12, bool = true 
